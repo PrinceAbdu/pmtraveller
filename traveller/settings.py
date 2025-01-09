a22e6dd017a5dcf556b85f8149584d34872a069c
abdullah  alakahsi <abdullahalakahsi@abdullahs-MacBook-Air.attlocal.net>
2025-01-09 15:04:11 -0600

    something

--- traveller/settings.py
+++ traveller/settings.py
@@ -123,7 +123,7 @@ import os  # Add this at the top of your file
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
