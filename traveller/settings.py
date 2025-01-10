1b1a5c438404950bc66f80fef8f8547c297fd9a1
abdullah  alakahsi <abdullahalakahsi@abdullahs-Air.attlocal.net>
2025-01-10 14:16:57 -0600

    Something

--- traveller/settings.py
+++ traveller/settings.py
@@ -123,11 +123,11 @@ import os  # Add this at the top of your file
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 AUTH_USER_MODEL = 'findingway.User'
 # 'DIRS': [os.path.join(BASE_DIR, 'temp/lates')],
 # Default primary key field type
