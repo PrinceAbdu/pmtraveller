fb77063230c039282e129b7086d23a15fb439852
abdullah  alakahsi <abdullahalakahsi@abdullahs-Air.attlocal.net>
2025-01-10 15:07:30 -0600

    Something

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 echo "Installing dependencies..."
-/opt/vercel/python3/bin/python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Creating staticfiles directory if it doesn't exist..."
 mkdir -p staticfiles
 
-echo "Listing current directory contents..."
-ls -la
-
-echo "Listing static directory contents..."
-ls -la static/
-
 echo "Collecting static files..."
-/opt/vercel/python3/bin/python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
+
+echo "Build completed."
